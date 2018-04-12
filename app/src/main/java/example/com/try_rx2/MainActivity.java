23aa2831734ddc2cec00afa2dade3ccc886be245
Timothy Yudi <timothyyudi@gmail.com>
2018-04-12 19:11:20 +0700

    init

---
+++ app/src/main/java/example/com/try_rx2/MainActivity.java
@@ -0,0 +1,55 @@
+package example.com.try_rx2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                emitter.onNext(20);
+                emitter.onComplete();
+            }
+        });
+
+        Observer observer = new Observer() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Object o) {
+                Toast.makeText(MainActivity.this, "data observed: "+(Integer)o, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+                Toast.makeText(MainActivity.this, "observation completed", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        observable.subscribe(observer);
+    }
+}
